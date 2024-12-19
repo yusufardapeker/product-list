5c2518f75488c132ee074662bad0882ad05b8bdd
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-12-19 12:13:30 +0300

    Initialize repository

---
+++ src/data.jsx
@@ -0,0 +1,155 @@
+import image1Thumbnail from "./images/image-waffle-thumbnail.jpg";
+import image1Mobile from "./images/image-waffle-mobile.jpg";
+import image1Tablet from "./images/image-waffle-tablet.jpg";
+import image1Desktop from "./images/image-waffle-desktop.jpg";
+
+import image2Thumbnail from "./images/image-creme-brulee-thumbnail.jpg";
+import image2Mobile from "./images/image-creme-brulee-mobile.jpg";
+import image2Tablet from "./images/image-creme-brulee-tablet.jpg";
+import image2Desktop from "./images/image-creme-brulee-desktop.jpg";
+
+import image3Thumbnail from "./images/image-macaron-thumbnail.jpg";
+import image3Mobile from "./images/image-macaron-mobile.jpg";
+import image3Tablet from "./images/image-macaron-tablet.jpg";
+import image3Desktop from "./images/image-macaron-desktop.jpg";
+
+import image4Thumbnail from "./images/image-tiramisu-thumbnail.jpg";
+import image4Mobile from "./images/image-tiramisu-mobile.jpg";
+import image4Tablet from "./images/image-tiramisu-tablet.jpg";
+import image4Desktop from "./images/image-tiramisu-desktop.jpg";
+
+import image5Thumbnail from "./images/image-baklava-thumbnail.jpg";
+import image5Mobile from "./images/image-baklava-mobile.jpg";
+import image5Tablet from "./images/image-baklava-tablet.jpg";
+import image5Desktop from "./images/image-baklava-desktop.jpg";
+
+import image6Thumbnail from "./images/image-meringue-thumbnail.jpg";
+import image6Mobile from "./images/image-meringue-mobile.jpg";
+import image6Tablet from "./images/image-meringue-tablet.jpg";
+import image6Desktop from "./images/image-meringue-desktop.jpg";
+
+import image7Thumbnail from "./images/image-cake-thumbnail.jpg";
+import image7Mobile from "./images/image-cake-mobile.jpg";
+import image7Tablet from "./images/image-cake-tablet.jpg";
+import image7Desktop from "./images/image-cake-desktop.jpg";
+
+import image8Thumbnail from "./images/image-brownie-thumbnail.jpg";
+import image8Mobile from "./images/image-brownie-mobile.jpg";
+import image8Tablet from "./images/image-brownie-tablet.jpg";
+import image8Desktop from "./images/image-brownie-desktop.jpg";
+
+import image9Thumbnail from "./images/image-panna-cotta-thumbnail.jpg";
+import image9Mobile from "./images/image-panna-cotta-mobile.jpg";
+import image9Tablet from "./images/image-panna-cotta-tablet.jpg";
+import image9Desktop from "./images/image-panna-cotta-desktop.jpg";
+
+export const productData = [
+	{
+		image: {
+			thumbnail: image1Thumbnail,
+			mobile: image1Mobile,
+			tablet: image1Tablet,
+			desktop: image1Desktop,
+		},
+		name: "Waffle with Berries",
+		category: "Waffle",
+		price: 6.5,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image2Thumbnail,
+			mobile: image2Mobile,
+			tablet: image2Tablet,
+			desktop: image2Desktop,
+		},
+		name: "Vanilla Bean Crème Brûlée",
+		category: "Crème Brûlée",
+		price: 7.0,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image3Thumbnail,
+			mobile: image3Mobile,
+			tablet: image3Tablet,
+			desktop: image3Desktop,
+		},
+		name: "Macaron Mix of Five",
+		category: "Macaron",
+		price: 8.0,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image4Thumbnail,
+			mobile: image4Mobile,
+			tablet: image4Tablet,
+			desktop: image4Desktop,
+		},
+		name: "Classic Tiramisu",
+		category: "Tiramisu",
+		price: 5.5,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image5Thumbnail,
+			mobile: image5Mobile,
+			tablet: image5Tablet,
+			desktop: image5Desktop,
+		},
+		name: "Pistachio Baklava",
+		category: "Baklava",
+		price: 4.0,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image6Thumbnail,
+			mobile: image6Mobile,
+			tablet: image6Tablet,
+			desktop: image6Desktop,
+		},
+		name: "Lemon Meringue Pie",
+		category: "Pie",
+		price: 5.0,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image7Thumbnail,
+			mobile: image7Mobile,
+			tablet: image7Tablet,
+			desktop: image7Desktop,
+		},
+		name: "Red Velvet Cake",
+		category: "Cake",
+		price: 4.5,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image8Thumbnail,
+			mobile: image8Mobile,
+			tablet: image8Tablet,
+			desktop: image8Desktop,
+		},
+		name: "Salted Caramel Brownie",
+		category: "Brownie",
+		price: 4.5,
+		amount: 1,
+	},
+	{
+		image: {
+			thumbnail: image9Thumbnail,
+			mobile: image9Mobile,
+			tablet: image9Tablet,
+			desktop: image9Desktop,
+		},
+		name: "Vanilla Panna Cotta",
+		category: "Panna Cotta",
+		price: 6.5,
+		amount: 1,
+	},
+];
---
+++ todos.md
@@ -0,0 +1,10 @@
+[x] Clear the redundants and add image and design sources
+[x] Install dev dependecies
+[x] Initialize the repository
+[] Add style files and Import font family
+[] Moving top bar
+[] Mobile layout with empty states
+[] Add cart logic
+[] Account logic
+[] Order confirmed logic
+[] Desktop layout
