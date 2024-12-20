7b0caeb47f894d473767e95386bb2a271003cf6a
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-12-20 22:35:48 +0300

    Code product list section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import "./styles/reset.css";
 import "./styles/main.scss";
 
 import { TopBar } from "./components/topBar";
+import { ProductList } from "./components/productList";
 
 function App() {
 	return (
 		<>
 			<TopBar />
+
+			<div className="container">
+				<ProductList />
+			</div>
 		</>
 	);
 }
